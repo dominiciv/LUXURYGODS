b270ec8961909558aa80ccd15158cdbfd22fab93
dominiciv <leonel.saldivia2017@gmail.com>
2025-02-20 11:20:57 -0300

    Update BackgroundImage.tsx

--- app/components/BackgroundImage.tsx
+++ app/components/BackgroundImage.tsx
@@ -4,7 +4,7 @@ export default function BackgroundImage() {
       className="absolute inset-0 z-0"
       style={{
         backgroundImage:
-          "url('https://hebbkx1anhila5yf.public.blob.vercel-storage.com/pagina_1920x1080-T3TOfBfZZFcRdXkauNWodQ28Yxag5b.png')",
+          "url('https://hebbkx1anhila5yf.public.blob.vercel-storage.com/pagina_1280x720-T3TOfBfZZFcRdXkauNWodQ28Yxag5b.png')",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
@@ -13,4 +13,3 @@ export default function BackgroundImage() {
     />
   )
 }
-
