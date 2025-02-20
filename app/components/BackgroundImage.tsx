044b15bc29fb38bd6577bb4cff87209d955dbfa4
dominiciv <leonel.saldivia2017@gmail.com>
2025-02-20 11:26:21 -0300

    Update BackgroundImage.tsx

--- app/components/BackgroundImage.tsx
+++ app/components/BackgroundImage.tsx
@@ -4,7 +4,7 @@ export default function BackgroundImage() {
       className="absolute inset-0 z-0"
       style={{
         backgroundImage:
-          "url('https://imgur.com/a/nSX4DBG')",
+          "url('https://hebbkx1anhila5yf.public.blob.vercel-storage.com/pagina_1920x1080-T3TOfBfZZFcRdXkauNWodQ28Yxag5b.png')",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
