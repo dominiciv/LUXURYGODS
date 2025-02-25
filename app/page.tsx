ec44ac531872e7a3c4c623a0b6633f25a61e5616
dominiciv <leonel.saldivia2017@gmail.com>
2025-02-25 10:47:18 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import RedirectButton from "./components/RedirectButton"
 import BackgroundImage from "./components/BackgroundImage"
-import Analytics from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
